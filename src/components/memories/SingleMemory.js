599b04909a33dbc5646358c2da6cc3bae707faaa
kath <khackethal.freelance@gmail.com>
2021-05-24 15:55:54 +0100

    map update

--- src/components/memories/SingleMemory.js
+++ src/components/memories/SingleMemory.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import axios from 'axios'
 import Error from '../auth/Error.js'
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom' 
 
+ 
+
+import { useRef, useState } from 'react'
+import mapboxgl from '!mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2F0aGFja2V0aGFsIiwiYSI6ImNrcDFnb2tsdDA4YXgydW90cXpjY2Fyc2oifQ.hMGNb-QI2MKxWxTRBNOIBg'
 
 function SingleMemory() {
   const [ memory, setSingleMemory ] = React.useState(null)
   const { id } = useParams()
   const [ isError, setIsError ] = React.useState(false)
   const isLoading = !memory && !isError
+  
+
+  //* For normal page content
 
   React.useEffect( () => {
     const getData = async () => {
@@ -23,7 +32,37 @@ function SingleMemory() {
     getData()
   },[id])
 
-  console.log(memory)
+
+
+  //* For map content-------------------
+
+  //* For map component
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng, setLng] = useState(-70.9)
+  const [lat, setLat] = useState(42.35)
+  const [zoom, setZoom] = useState(9)
+
+  // initial mount
+  React.useEffect(() => {
+    if (map.current) return // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom,
+    })
+  })
+
+  // move map
+  React.useEffect(() => {
+    if (!map.current) return // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4))
+      setLat(map.current.getCenter().lat.toFixed(4))
+      setZoom(map.current.getZoom().toFixed(2))
+    })
+  })
 
 
   return (
@@ -36,6 +75,13 @@ function SingleMemory() {
           <h2>{memory.location}</h2>
           <p>{memory.description}</p>
           <img height ="540px" width="810px"  src={memory.imageUrl} alt={memory.name} />
+
+          <div>
+            <div className="sidebar"> nope
+            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div ref={mapContainer} className="map-container" />
+          </div>
         </>
       )}
 
