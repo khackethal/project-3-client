2a98e43e283969b342c26a4d9093153498350384
Anthony Graham <anthony@databoy.tv>
2021-05-27 17:54:23 +0200

    update

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,43 +1,94 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { isAuthenticated, removeToken } from '../../lib/auth'
+
 function Navbar() {
+
+  const [isOpen, setIsOpen] = React.useState(false)
+  const isLogged = isAuthenticated()
+
+  // React.useEffect({
+
+  // },[])
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const handleLogout = () => {
+    removeToken()
+  }
+
   return (
 
     <nav className="navbar is-info">
+      <>{console.log('isLogged: ', isLogged)}</>
       <div className="container">
         <div className="navbar-brand">
           <a className="navbar-item">
             <img src="https://i.pinimg.com/originals/33/e6/3d/33e63d5adb0da6b303a83901c8e8463a.png" alt="logo"></img>
           </a>
-          <span className="navbar-burger" data-target="navbarMenuHeroB">
+
+          <span
+            className={`navbar-burger ${isOpen ? 'is-active' : ''}`}
+            data-target="navbarMenuHeroB"
+            onClick={handleClick}
+          >
             <span></span>
             <span></span>
             <span></span>
           </span>
+
         </div>
-        <div id="navbarMenuHeroB" className="navbar-menu">
+        <div
+          id="navbarMenuHeroB"
+          className={`navbar-menu ${isOpen ? 'is-active' : '' }`}
+        >
           <div className="navbar-end">
-            <a className="navbar-item is-active">
-              <Link to="/" > Home</Link>
-            </a>
+
             <a className="navbar-item">
-              <Link to="/memories" > Memory Index</Link>
+              <Link to="/" >Home</Link>
             </a>
+
             <a className="navbar-item">
-              <Link to="/memories/map" > Memory Map</Link>
+              <Link to="/memories" >Memory Index</Link>
             </a>
+
+            <a className="navbar-item">
+              <Link to="/memories/map" >Memory Map</Link>
+            </a>
+
             <a className="navbar-item">
               <Link to="/about" >About</Link> 
             </a>
-            <a className="navbar-item">
-              <Link to="/register" >Register</Link> 
-            </a>
-            <a className="navbar-item">
-              <Link to="/login" >Login</Link> 
-            </a>
+
+            {!isLogged &&
+              <a className="navbar-item">
+                <Link to="/register" >Register</Link> 
+              </a>
+            }
+
+            {!isLogged &&
+              <a className="navbar-item">
+                <Link to="/login" >Login</Link> 
+              </a>
+            }
+
+            {isLogged &&
+              <a className="navbar-item">
+                <Link
+                  onClick={handleLogout}
+                  to="/"
+                >
+                  Logout
+                </Link> 
+              </a>
+            }
+
             <a className="navbar-item">
               <Link to="/api" >Api Test</Link> 
             </a>
+
           </div>
         </div>
       </div>
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -12,27 +12,22 @@ export function removeToken() {
 
 function getPayload() {
   const token = getToken()
-  console.log('token: ', token)
 
   if (!token) return false
   const parts = token.split('.')
-  console.log('parts: ', parts)
   if (parts.length < 3) return false
   return JSON.parse(atob(parts[1]))
 }
 
 export function isAuthenticated() {
   const payload = getPayload()
-  console.log('payload: ', payload)
   if (!payload) return false
   const now = Math.round(Date.now() / 1000)
-  console.log('now: ', now)
   return now < payload.exp
 }
 
 export function isOwner(userId) {
   const payload = getPayload()
-  console.log('payload: ', payload)
   if (!isAuthenticated()) return false
   return payload.sub === userId
 }
