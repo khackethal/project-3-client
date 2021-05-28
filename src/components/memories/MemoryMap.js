8acd9af2a22d8614ce434f6717f1e3e6daa62989
kath <khackethal.freelance@gmail.com>
2021-05-28 15:38:52 +0100

    comments & deleting comments on sinlge memory page

--- src/components/memories/MemoryMap.js
+++ src/components/memories/MemoryMap.js
@@ -9,7 +9,7 @@ function MemoryMap() {
   const [ memories, setAllMemories ] = useState(null)
   const [ isError, setIsError ] = useState(false)
   const isLoading = !memories && !isError
-  const [ searchTerm, setSerachTerm ] = useState('')
+  const [ searchTerm, setSearchTerm ] = useState('')
 
   useEffect(() => {
     const getData = async () => {
@@ -38,7 +38,7 @@ function MemoryMap() {
 
   //* search functions
   const handleInput = (e) => {
-    setSerachTerm(e.target.value)
+    setSearchTerm(e.target.value)
   }
 
 
